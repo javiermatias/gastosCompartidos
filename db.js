0a20952303826c89e4c46fac877ed104f55fc583
javiermatias <javierjimenez78@gmail.com>
2020-01-16 22:34:40 -0300

    subiendo

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
 
-/*  // First you need to create a connection to the db
- const connection = mysql.createConnection({
+ // First you need to create a connection to the db
+ const connection = mysql.createPool({
+  connectionLimit : 10,
    host: 'mysql5022.site4now.net',
    user: 'a34972_evento',
    password: 'tutuca.99',   
@@ -15,16 +16,7 @@ const mysql = require('mysql');
      return;
    }
    console.log('Connection established');
- }); */
-
- var db_config = {
-  host: 'mysql5022.site4now.net',
-    user: 'a34972_evento',
-    password: 'tutuca.99',
-    database: 'db_a34972_evento'
-};
-
-var connection;
+ });
  
 /*  connection.end((err) => {
    // The connection is terminated gracefully
@@ -32,32 +24,11 @@ var connection;
    // before sending a COM_QUIT packet to the MySQL server.
  }); */
 
- function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
 
 
 
 
 
+ 
 
  module.exports = connection;
