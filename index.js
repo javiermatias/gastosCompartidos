f48393849243c2f9a933f12fa5252a13095380ed
javiermatias <javierjimenez78@gmail.com>
2020-01-16 21:26:00 -0300

    change

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ var port = process.env.PORT || 8080;
 //app.get('/', (req, res) => res.send('Bienvenido a gastos Compartidos'));
 // Launch app to listen to specified port
 // Import routes
-
 // Serve static files....
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
 
 // Use Api routes in the App
 app.use('/api', apiRoutes)
